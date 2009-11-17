b6c5ce877edc529b4eb280150b1b8e119ac4ec79
seba.melzi <seba.melzi@bdc3f3a8-1fb0-11de-9909-f37ff7dbbc12>
2009-11-17 00:47:24 +0000

    :)

--- simulatorGUI/src/objetoVisual/accionVisual/accionDejarGrupoVisual.java
+++ simulatorGUI/src/objetoVisual/accionVisual/accionDejarGrupoVisual.java
@@ -1,5 +1,11 @@
 package objetoVisual.accionVisual;
 
+import java.awt.Dialog;
+
+import objetoVisual.listaObjetos;
+import visSim.dialogosAcciones.dialogoAccionApagar;
+import visSim.dialogosAcciones.dialogoAccionBase;
+import visSim.dialogosAcciones.dialogoAccionDejarGrupo;
 import Equipos.Equipo;
 import Proyecto.Acciones.Accion;
 import Proyecto.Acciones.AccionDejarGrupo;
@@ -31,6 +37,13 @@ public class accionDejarGrupoVisual extends accionVisual{
 	
 	public Accion createAccion(Equipo e) {
 		return new AccionDejarGrupo(new DireccionIPv4(this.direccionGrupo),e.getInterfaz(this.interfaz));
+	}
+	
+	public dialogoAccionBase createDialog(Dialog parent, int xCentral, int yCentral, listaObjetos lista) {
+		dialogoAccionDejarGrupo dlgAccion = new dialogoAccionDejarGrupo(parent,xCentral, yCentral, lista);
+		dlgAccion.setInstante(this.instante);
+		dlgAccion.setEquipo(this.equipo);
+		return dlgAccion;
 	}
 	
 }
