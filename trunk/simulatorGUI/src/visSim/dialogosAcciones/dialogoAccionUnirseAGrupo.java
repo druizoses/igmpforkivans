65edc11abcb1c915d572dd4c687be32659c8adc7
seba.melzi <seba.melzi@bdc3f3a8-1fb0-11de-9909-f37ff7dbbc12>
2009-11-17 00:47:48 +0000

    :)

--- trunk/simulatorGUI/src/visSim/dialogosAcciones/dialogoAccionUnirseAGrupo.java
+++ trunk/simulatorGUI/src/visSim/dialogosAcciones/dialogoAccionUnirseAGrupo.java
@@ -97,4 +97,12 @@ public class dialogoAccionUnirseAGrupo extends dialogoAccionBase {
 		return true;
 	}
 	
+	public void setInstante(int instante) {
+		this.txtInstante.setText(String.valueOf(instante));
+	}
+
+	public void setEquipo(String equipo) {
+		this.equiposDisponibles.setSelectedItem(equipo);
+	}
+	
 }
